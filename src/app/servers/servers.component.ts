f8bd57466e5cbe895b1191086de986b00a769311
Vaerilius <tjohannest@gmail.com>
2019-04-20 19:57:31 +0300

    ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -16,6 +16,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created';
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -25,13 +26,13 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
     this.serverName = (<HTMLInputElement>event.target).value;
   }
-
 
   ngOnInit() {
   }
