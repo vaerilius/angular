39aa0d1db90943139ca49af3512ce3e30f33a6fd
Vaerilius <tjohannest@gmail.com>
2019-06-27 15:23:15 +0300

    build 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
+// @ts-ignore
 import { BrowserModule } from '@angular/platform-browser';
+// @ts-ignore
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+// @ts-ignore
 import { FormsModule } from '@angular/forms';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContainerComponent } from './container/container.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
+
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    NavbarComponent,
+    ContainerComponent,
+    FooterComponent,
+    ContactComponent
+
   ],
   imports: [
     BrowserModule,
